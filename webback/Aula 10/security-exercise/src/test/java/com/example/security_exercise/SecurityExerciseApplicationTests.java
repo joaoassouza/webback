8103affd8590e667c3793e25e32d1b0aed5dc759
João souza <joaodealmedassouza@gmail.com>
2025-05-22 21:37:13 -0300

    seting secutiry exercise

---
+++ webback/Aula 10/security-exercise/src/test/java/com/example/security_exercise/SecurityExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
